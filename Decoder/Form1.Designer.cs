6e9d40c1dd437f3ab2cef50a4c20d238600b9772
Daniel Merva <merva.daniel@outlook.cz>
2019-06-05 19:19:53 +0200

    Add project files.

---
+++ Decoder/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Decoder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNormalni = new System.Windows.Forms.Button();
+            this.tbNormalniText = new System.Windows.Forms.TextBox();
+            this.tbMorseovka = new System.Windows.Forms.TextBox();
+            this.btnMorseovka = new System.Windows.Forms.Button();
+            this.btnBeep = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnNormalni
+            // 
+            this.btnNormalni.Location = new System.Drawing.Point(107, 96);
+            this.btnNormalni.Name = "btnNormalni";
+            this.btnNormalni.Size = new System.Drawing.Size(75, 23);
+            this.btnNormalni.TabIndex = 0;
+            this.btnNormalni.Text = "Odeslat";
+            this.btnNormalni.UseVisualStyleBackColor = true;
+            this.btnNormalni.Click += new System.EventHandler(this.btnNormalni_Click);
+            // 
+            // tbNormalniText
+            // 
+            this.tbNormalniText.Location = new System.Drawing.Point(13, 13);
+            this.tbNormalniText.Multiline = true;
+            this.tbNormalniText.Name = "tbNormalniText";
+            this.tbNormalniText.Size = new System.Drawing.Size(259, 77);
+            this.tbNormalniText.TabIndex = 1;
+            // 
+            // tbMorseovka
+            // 
+            this.tbMorseovka.Location = new System.Drawing.Point(13, 125);
+            this.tbMorseovka.Multiline = true;
+            this.tbMorseovka.Name = "tbMorseovka";
+            this.tbMorseovka.Size = new System.Drawing.Size(259, 86);
+            this.tbMorseovka.TabIndex = 2;
+            // 
+            // btnMorseovka
+            // 
+            this.btnMorseovka.Location = new System.Drawing.Point(107, 226);
+            this.btnMorseovka.Name = "btnMorseovka";
+            this.btnMorseovka.Size = new System.Drawing.Size(75, 23);
+            this.btnMorseovka.TabIndex = 3;
+            this.btnMorseovka.Text = "Odeslat";
+            this.btnMorseovka.UseVisualStyleBackColor = true;
+            this.btnMorseovka.Click += new System.EventHandler(this.btnMorseovka_Click);
+            // 
+            // btnBeep
+            // 
+            this.btnBeep.Location = new System.Drawing.Point(227, 226);
+            this.btnBeep.Name = "btnBeep";
+            this.btnBeep.Size = new System.Drawing.Size(45, 22);
+            this.btnBeep.TabIndex = 4;
+            this.btnBeep.Text = "Beep";
+            this.btnBeep.UseVisualStyleBackColor = true;
+            this.btnBeep.Click += new System.EventHandler(this.btnBeep_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnBeep);
+            this.Controls.Add(this.btnMorseovka);
+            this.Controls.Add(this.tbMorseovka);
+            this.Controls.Add(this.tbNormalniText);
+            this.Controls.Add(this.btnNormalni);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnNormalni;
+        private System.Windows.Forms.TextBox tbNormalniText;
+        private System.Windows.Forms.TextBox tbMorseovka;
+        private System.Windows.Forms.Button btnMorseovka;
+        private System.Windows.Forms.Button btnBeep;
+    }
+}
+
---
+++ Decoder/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+namespace Decoder
+{
+    public partial class Form1 : Form
+    {
+        public static Dictionary<char, String> morseovka = new Dictionary<char, String>()
+        {
+            {'A',".-"},
+            {'B',"-..."},
+            {'C',"-.-."},
+            {'D',"-.."},
+            {'E',"." },
+            {'F',"..-."},
+            {'G',"--."},
+            {'H',"...."},
+            {'a',"----"}, // nahrazuje CH
+            {'I',".."},
+            {'J',".---"},
+            {'K',"-.-"},
+            {'L',".-.."},
+            {'M',"--"},
+            {'N',"-."},
+            {'O',"---"},
+            {'P',".--."},
+            {'Q',"--.-"},
+            {'R',".-."},
+            {'S',"..."},
+            {'T',"-"},
+            {'U',"..-"},
+            {'V',"...-"},
+            {'W',".--"},
+            {'X',"-..-"},
+            {'Y',"-.--"},
+            {'Z',"--.."}
+
+        };
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNormalni_Click(object sender, EventArgs e)
+        {
+            tbMorseovka.Text = "";
+
+            String text = tbNormalniText.Text.ToUpper();
+            if (text.Contains("CH"))
+            {
+                text = text.Replace("CH","a");
+            }
+               
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (i > 0)
+                {
+                    tbMorseovka.Text += "/";
+                }
+
+                char znak = text[i];
+                if (morseovka.ContainsKey(znak))
+                {
+                    tbMorseovka.Text += morseovka[znak];
+                }
+
+            }
+            
+        }
+
+        private void btnMorseovka_Click(object sender, EventArgs e)
+        {
+            tbNormalniText.Text = "";
+            tbMorseovka.Text += "/";
+            int count = 0;
+            foreach (char c in tbMorseovka.Text)
+                if (c == '/') count++;
+
+            string[] hodnoty = tbMorseovka.Text.Split('/');
+            for (int i = 0; i <= count; i++)
+            {
+                String hodnota = hodnoty[i];
+                if (morseovka.ContainsValue(hodnota))
+                {
+                    if (!(hodnota == "----"))
+                        tbNormalniText.Text += morseovka.FirstOrDefault(p => p.Value == hodnota).ToString().Substring(0, 2).Trim('[').ToLower();
+                    if (hodnota == "----")
+                        tbNormalniText.Text += "ch";
+                }
+
+            }
+
+
+        }
+
+        private void btnBeep_Click(object sender, EventArgs e)
+        {
+
+            foreach (char item in tbMorseovka.Text)
+            {
+                if (item == '.')
+                {
+                    System.Media.SoundPlayer soundDot = new System.Media.SoundPlayer(Properties.Resources.dot);
+                    soundDot.Play();
+                }
+                if (item == '-')
+                {
+                    System.Media.SoundPlayer soundDash = new System.Media.SoundPlayer(Properties.Resources.dash);
+                    soundDash.Play();
+                }
+            }
+        }
+    }
+    }
+
